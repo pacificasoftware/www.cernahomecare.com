9425cbc81bc4671a8993b5d27096147fd2db4aa2
Paul <paul.amand@gmail.com>
2026-04-22 14:00:07 -0700

    Default Change

--- app/careers/page.tsx
+++ app/careers/page.tsx
@@ -1,3 +1,11 @@
 ﻿import "./careers.css";
 import Link from "next/link";
 import Image from "next/image";
+
+export default function CareersPage() {
+    return (
+        <main>
+            <h1>Careers</h1>
+        </main>
+    );
+}
